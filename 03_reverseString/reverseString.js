ae7b1202da10c50058924e026ca0783620a445e8
Diana Heng <82561944+DianaHeng@users.noreply.github.com>
2023-06-07 23:00:40 +0800

    Completed 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,18 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    // const splittedStr = str.split("");
+    // let reverseArray = splittedStr.reverse();
+    // const reversedStr = reverseArray.join("");
+    // return reversedStr;
 
+    //return str.split("").reverse().join("")
+
+    //or [use decrementing for loop]
+
+    let newStr = "";
+    for (let i = str.length-1; i>=0; i--){
+        newStr += str[i]
+    }
+    return newStr;
 };
 
 // Do not edit below this line
