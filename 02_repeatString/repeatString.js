6ee2636a86863e792ca29fa151536d2f549c881f
Diana Heng <82561944+DianaHeng@users.noreply.github.com>
2023-06-07 22:43:27 +0800

    Completed 01_helloWorld, 02_repeatString, 013_primeNumber

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+let repeatedStr = "";
+if (num <0) {return "ERROR"};
+while (num>0){
+    repeatedStr += string;
+    num--;
+}
+return repeatedStr;
 };
 
 // Do not edit below this line
